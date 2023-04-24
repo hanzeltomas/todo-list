2a7450224939b03cf24fde6cfd4214958f8a3f73
hanzeltomas <hanzel.toma@gmail.com>
2023-04-24 09:58:06 +0200

    fix: missing key prop

--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -28,11 +28,8 @@ const TodosList: React.FC = () => {
         <div className='p-5 bg-[#1e293b] w-96 rounded-xl text-center relative'>
           <ul>
             {todoLists.map((todoList) => (
-              <Link href={`todo/${todoList.id}`}>
-                <li
-                  className='text-sky-400 block w-full cursor-pointer rounded-lg p-4 transition duration-500 hover:bg-neutral-100 hover:text-neutral-500 focus:bg-neutral-100 focus:text-neutral-500 focus:ring-0 dark:hover:bg-neutral-600 dark:hover:text-neutral-200 dark:focus:bg-neutral-600 dark:focus:text-neutral-200'
-                  key={todoList.id}
-                >
+              <Link key={todoList.id} href={`todo/${todoList.id}`}>
+                <li className='text-sky-400 block w-full cursor-pointer rounded-lg p-4 transition duration-500 hover:bg-neutral-100 hover:text-neutral-500 focus:bg-neutral-100 focus:text-neutral-500 focus:ring-0 dark:hover:bg-neutral-600 dark:hover:text-neutral-200 dark:focus:bg-neutral-600 dark:focus:text-neutral-200'>
                   <h2>{todoList.name}</h2>
                 </li>
               </Link>
